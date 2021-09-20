12e7ede6cf3c19adf9b0a4b8432f36ace82d4a00
Supernova1114 <supernovawarriors128@gmail.com>
2021-09-19 18:47:48 -0700

    Fixing the mess 1

---
+++ Mini-Jam-89-Pumpkin/Assets/Scripts/TreeCheckCollision.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeCheckCollision : MonoBehaviour
+{
+    public Transform player;
+    public SpriteRenderer layers;
+
+    private void Start()
+    {
+        layers = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        if ((transform.position.y - 4.1) > (player.transform.position.y - 1))
+        {
+            gameObject.GetComponent<CircleCollider2D>().enabled = false;
+            layers.GetComponent<SpriteRenderer>().sortingOrder = -1;
+        }
+        else
+        {
+            gameObject.GetComponent<CircleCollider2D>().enabled = true;
+            layers.GetComponent<SpriteRenderer>().sortingOrder = 1;
+        }
+    }
+}
